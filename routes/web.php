8146eb86c6d2a86f063c58d5f28debcdd539a715
Simon Vrachliotis <simon@simonswiss.com>
2025-07-16 11:26:41 +1000

    Add demos navigation on welcome.php route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-Route::view('/scrolly-telling', 'scrolly-telling');
-Route::view('/scroll-gutter', 'scroll-gutter');
-Route::view('/css-carousel', 'css-carousel');
-Route::view('/marquee', 'marquee');
+
+// Hacky way to get auto-navigation working in the frontend
+Route::prefix('')->name('demos.')->group(function () {
+ Route::view('/scrolly-telling', 'scrolly-telling')->name('scrolly-telling');
+ Route::view('/scroll-gutter', 'scroll-gutter')->name('scroll-gutter');
+ Route::view('/css-carousel', 'css-carousel')->name('css-carousel');
+ Route::view('/marquee', 'marquee')->name('marquee');
+});
